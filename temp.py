be632ac8715134de0b611937cdc35209ee6c983f
ani <anirudh77.23@ichat.sp.edu.sg>
2025-01-31 16:35:20 +0800

    added led to temp.py

--- temp.py
+++ temp.py
@@ -7,6 +7,7 @@ from time import sleep
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18,GPIO.OUT) #set GPIO 18 as output
+GPIO.setup(24,GPIO.OUT) #set GPIO 24 as output
 
 instance = dht11.DHT11(pin=21) #read data using pin 21
 
@@ -22,13 +23,17 @@ try:
 
         if result.temperature > 26:
             GPIO.output(18,1)
+            GPIO.output(24,1)
             sleep(1)
             GPIO.output(18,0)
+            GPIO.output(24,0)
             sleep(1)
         if result.temperature> 27:
             GPIO.output(18,1)
+            GPIO.output(24,1)
             sleep(4)
             GPIO.output(18,0)
+            GPIO.output(24,0)
             sleep(1)
             
             
