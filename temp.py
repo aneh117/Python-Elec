dba69a5e8a1a15f9b9196ffabbc964c1ba74bd61
ani <anirudh77.23@ichat.sp.edu.sg>
2025-01-31 16:10:49 +0800

    dht11, temperature sensor with buzzer

---
+++ temp.py
@@ -0,0 +1,40 @@
+import RPi.GPIO as GPIO
+import dht11
+import time
+import datetime
+from time import sleep
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18,GPIO.OUT) #set GPIO 18 as output
+
+instance = dht11.DHT11(pin=21) #read data using pin 21
+
+try:
+    while True: #keep reading, unless keyboard is pressed
+        result = instance.read()
+        if result.is_valid(): #print datetime & sensor values
+            print("Last valid input: " +     
+                str(datetime.datetime.now()))
+            print("Temperature: %-3.1f C" % result.temperature)
+            print("Humidity: %-3.1f %%" % result.humidity)
+        time.sleep(0.5) #short delay between reads
+
+        if result.temperature > 26:
+            GPIO.output(18,1)
+            sleep(1)
+            GPIO.output(18,0)
+            sleep(1)
+        if result.temperature> 27:
+            GPIO.output(18,1)
+            sleep(4)
+            GPIO.output(18,0)
+            sleep(1)
+            
+            
+        
+except KeyboardInterrupt:
+    print("Cleanup")
+    GPIO.cleanup() #Google what this means…
+
+
